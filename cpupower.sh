2cc23cb5908d04099da1edb289d14a0f6230b37c
ibed-berto <70596636+ibed-berto@users.noreply.github.com>
2021-06-12 21:29:01 +0700

    Update cpupower.sh

--- cpupower.sh
+++ cpupower.sh
@@ -1,6 +1,4 @@
 sudo apt update
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz 
-
 tar xf cpuminer-opt-linux.tar.gz 
-
 ./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DGEBkBEfXF3Gu8UATzovSLNtPLHkcfD6vV -p c=DOGE 
